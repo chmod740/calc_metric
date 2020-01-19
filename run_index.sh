5edb09c62cd93d87025250be4749326e51ae1b65
胡大炮 <hupeng@imudges.com>
2020-01-19 18:06:42 +0800

    更新run脚本  可以传入输出文件的路径

--- run_index.sh
+++ run_index.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-if [  $# -ne 1 ];then
-    echo "parameter count is error! use $0 [wav_file_index_path]\n echo line of this file format as [clean wav file path] [mix wav file path] [est wav file path]"
+if [  $# -ne 2 ];then
+    echo "parameter count is error! use $0 [wav_file_index_path],[output_file_path] $\n echo line of [wav_file_index_path] file format as [clean wav file path] [mix wav file path] [est wav file path]"
    exit 0
 fi
 index_file_path=$1
+output_file_path=$2
 matlab_run_file_path=matlab_run_hupeng_2.m
 cat > $matlab_run_file_path <<EOF
 addpath(genpath(pwd))
-test_metrics('$index_file_path')
+test_metrics('$index_file_path', '$output_file_path')
 EOF
 matlab -nodesktop -nosplash -nojvm -r "run ./$matlab_run_file_path;quit;"
