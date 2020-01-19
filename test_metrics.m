a70a483791d41d0a0ad8a83d591ab6a450562e89
胡大炮 <hupeng@imudges.com>
2020-01-19 18:12:08 +0800

    Update test_metrics.m

--- test_metrics.m
+++ test_metrics.m
@@ -1,4 +1,4 @@
-function [] = test_metrics( index_file_path )
+function [] = test_metrics( index_file_path， out_file_path )
 %MAIN Summary of this function goes here
 %   Detailed explanation goes here
     fp = fopen(index_file_path, 'r');
@@ -44,6 +44,9 @@ function [] = test_metrics( index_file_path )
     sum_est_sdr = sum_est_sdr / count;
     
     json_str = sprintf('{"mix_stoi":%.6f, "mix_pesq":%.6f, "mix_snr":%.6f, "mix_sdr": %.6f, "est_stoi":%.6f, "est_pesq":%.6f, "est_snr":%.6f, "est_sdr": %.6f}', sum_mix_stoi, sum_mix_pesq, sum_mix_snr, sum_mix_sdr, sum_est_stoi, sum_est_pesq, sum_est_snr, sum_est_sdr);
-    disp(json_str);
+    % disp(json_str);
+    fid = fopen(out_file_path,'w');
+    fprintf(fid,'%s\n',json_str); 
+    fclose(fid);
 end
 
